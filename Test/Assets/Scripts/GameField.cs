6c06d04091b7ec7a6e83f272b4ae470a5bc399f5
DESKTOP-N9U7COP\Dusheba <dusheba.andrey@gmail.com>
2017-02-16 23:59:42 +0200

    Update

---
+++ Test/Assets/Scripts/GameField.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GameField
+    {
+        public int SideSize { get; private set; }
+        public int ObstacleCount { get; private set; }
+        public int[] ObstacleSize { get; private set; }
+        public int[,] Map { get; private set; }
+
+        public List<Obstacle> Obstacles { get; private set; }
+
+        public GameField(int sideSize, int obstacleCount, int[] obstacleSize, int[,] map)
+        {
+            this.SideSize = sideSize;
+            this.ObstacleCount = obstacleCount;
+            this.ObstacleSize = obstacleSize;
+            this.Map = map;
+
+            Obstacles = new List<Obstacle>();
+        }
+
+        //Move TO SEPARATE SERVICE CLASS
+        public void PushObstacle(int x, int y, int size)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            if (x < Map.GetLength(0) - 1 && Map[x + 1, y] == Map[x, y])
+            {
+                cube.transform.position = new Vector3(Map[x, y], Map[x, y] + Map.GetLength(1) / 2, -5);
+                cube.transform.localScale = new Vector3(1, size, 1);
+            }
+
+            if (y < Map.GetLength(1) - 1 && Map[x, y + 1] == Map[x, y])
+            {
+                cube.transform.position = new Vector3(Map[x, y], Map[x, y] + Map.GetLength(1) / 2, -5);
+                cube.transform.localScale = new Vector3(size, 1, 1);
+            }
+
+            Obstacle instance = new Obstacle()
+            {
+                Id = Obstacles.Count, // hardcode
+                IsVertical = false, // hardcode
+                Object = cube,
+                Size = size,
+                xPos = x,
+                yPos = y
+            };
+
+            Obstacles.Add(instance);
+        }
+
+        public void InstantiateObstacles()
+        {
+            //foreach (var obstacle in Obstacles)
+            //    //Instantiate(obstacle);
+        }
+
+    }
+}
---
+++ Test/Assets/Scripts/GenerateLevel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+public class GenerateLevel : MonoBehaviour
+{
+
+    
+
+    void Start()
+    {
+        GameField gameField = new GameField(sideSize: 5, obstacleCount: 5, obstacleSize: new int[5], map: new int[0, 0]);
+            
+
+        var service = new AssemblyCSharp.MapService();
+        service.MapInit(gameField.Map);
+        service.MapPlayer(gameField.Map);
+
+        service.PrintMap(gameField.Map);
+
+
+        CreateObjects(gameField);
+    }
+    void CreateObjects(GameField gameField)
+    {
+        for (int i = 0; i <=0; i++)
+        {
+            for (int j = 0; j < gameField.SideSize; j++)
+            {
+                for (int g = 0; g < gameField.SideSize; g++)
+                {
+                    if (gameField.Map[j, g] == i)
+                    {
+                        gameField.PushObstacle( j, g, i == 0 ? 2 : gameField.ObstacleSize[i - 1]);
+
+                        goto next;
+                    }
+                }
+            }
+        next:
+            Debug.Log(i + " has been instantiated");
+        }
+    }
+      
+}
+		
